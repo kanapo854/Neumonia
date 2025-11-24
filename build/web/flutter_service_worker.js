5891fc1ef6706badf012b635624ff0bc90af777a
kanapo854 <quispechaconvalheryleaylean@gmail.com>
2025-11-23 23:13:31 -0400

    Update: Recompilar web con tamaño de imagen corregido

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "afe122163f156462708ef41b0898ffa8
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "74113f2e24ef25957a5411afb90ea7a0",
+"flutter_bootstrap.js": "24a75aed5577519ad637714826bbc7d6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "7e049bc1dc3b824f26da3432bae19219",
 "/": "7e049bc1dc3b824f26da3432bae19219",
-"main.dart.js": "50e107954ae5c04b23ab0f1607d02682",
+"main.dart.js": "d5b9f7734a86862ba45e9de041576a78",
 "manifest.json": "97021fd68c23e0343dacaa66b6945e81",
 "version.json": "10aac9edd5d069b2628ac31e71536d9a"};
 // The application shell files that are downloaded before a service worker can
